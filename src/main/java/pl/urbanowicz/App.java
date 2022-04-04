91964084e2a971a52f9c9e2d16eb36566eb975fb
Tomasz Urbanowicz <t.urbanowicz94@gmail.com>
2022-04-04 12:45:54 +0200

    Modified .gitignore file

--- src/main/java/pl/urbanowicz/App.java
+++ src/main/java/pl/urbanowicz/App.java
@@ -1,9 +1,7 @@
 package pl.urbanowicz;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
